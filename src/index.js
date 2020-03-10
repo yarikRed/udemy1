90f607ed34d3d2f80e271f4468d44b323686837a
Yarik Red <yarik.red@gmail.com>
2020-03-10 12:12:39 +0200

    Section 2: Building content with JSX code review

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = function() {
-    const buttonText = () => 'Submit';
-    const labelText = () => 'Enter name:';
+    
     return (
-        <div>
-            <label classname="label" htmlFor="name">{labelText()}</label>
-            <input id="name" type="text" />
-
-            <button style={{ backgroundColor: 'blue', color: 'white'}}>
-                {buttonText()}
-            </button>
+        <div className="ui container comments">
+            <CommentDetail author={faker.name.firstName()} timeAgo={faker.date.past()}/>
+            <CommentDetail author={faker.name.firstName()} timeAgo={faker.date.past()}/>
+            <CommentDetail author={faker.name.firstName()} timeAgo={faker.date.past()}/>
+            <CommentDetail author={faker.name.firstName()} timeAgo={faker.date.past()}/>
+            <CommentDetail author={faker.name.firstName()} timeAgo={faker.date.past()}/>
+            <CommentDetail author={faker.name.firstName()} timeAgo={faker.date.past()}/>
         </div>
     );
 };
